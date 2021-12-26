dbe028c52994685dfa0a78334fecccaa0c205ed9
Jouni Sunnari <dz4tune@gmail.com>
2021-12-26 17:54:42 +0300

    replaced templates with benchmark's ranges

--- bm_integrate_image.cpp
+++ bm_integrate_image.cpp
@@ -10,7 +10,6 @@ static void BM_integrate(benchmark::State& state)
     }
 }
 
-template<unsigned Threads>
 static void BM_integrate_inplace(benchmark::State& state)
 {
     cv::Mat3d m(1000, 1000);
@@ -18,7 +17,7 @@ static void BM_integrate_inplace(benchmark::State& state)
         state.PauseTiming();
         cv::randu(m, -1000, 1000);
         state.ResumeTiming();
-        integrate_inplace(m, Threads);
+        integrate_inplace(m, state.range(0));
     }
 }
 
@@ -32,39 +31,36 @@ static void BM_opencv_integrate(benchmark::State& state)
     }
 }
 
-template<unsigned Threads>
 static void BM_row_partial_sums(benchmark::State& state)
 {
     cv::Mat3d m(1000, 1000);
     cv::randu(m, -1000.0, 1000.0);
     for (auto _ : state) {
         cv::Mat res;
-        row_partial_sums(m, Threads);
+        row_partial_sums(m, state.range(0));
     }
 }
 
-template<unsigned Threads>
 static void BM_col_partial_sums(benchmark::State& state)
 {
     cv::Mat3d m(1000, 1000);
     cv::randu(m, -1000.0, 1000.0);
     for (auto _ : state) {
         cv::Mat res;
-        col_partial_sums(m, Threads);
+        col_partial_sums(m, state.range(0));
     }
 }
 
+static void num_threads(benchmark::internal::Benchmark* b) {
+    for (size_t i = 0; i < std::thread::hardware_concurrency(); ++i) {
+        b->Arg(i+1);
+    }
+}
 
-BENCHMARK_TEMPLATE1(BM_integrate_inplace, 1);
-BENCHMARK_TEMPLATE1(BM_integrate_inplace, 2);
-BENCHMARK_TEMPLATE1(BM_integrate_inplace, 3);
+BENCHMARK(BM_integrate_inplace)->Apply(num_threads);
 BENCHMARK(BM_integrate);
 BENCHMARK(BM_opencv_integrate);
-BENCHMARK_TEMPLATE1(BM_row_partial_sums, 1);
-BENCHMARK_TEMPLATE1(BM_row_partial_sums, 2);
-BENCHMARK_TEMPLATE1(BM_row_partial_sums, 3);
-BENCHMARK_TEMPLATE1(BM_col_partial_sums, 1);
-BENCHMARK_TEMPLATE1(BM_col_partial_sums, 2);
-BENCHMARK_TEMPLATE1(BM_col_partial_sums, 3);
+BENCHMARK(BM_row_partial_sums)->Apply(num_threads);
+BENCHMARK(BM_col_partial_sums)->Apply(num_threads);
 
 BENCHMARK_MAIN();
